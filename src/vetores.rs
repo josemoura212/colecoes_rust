3557b194ab3fb653fa07f34cda64d9b27b00f7c4
José Augusto <josemoura212@gmail.com>
2024-01-08 18:06:48 -0300

    Refactor main.rs to use a separate module for vectors

---
+++ src/vetores.rs
@@ -0,0 +1,19 @@
+pub fn vetores() {
+    let lista: [u8; 5] = [1, 2, 3, 4, 5];
+
+    println!("Valor na posição 2: {}\n", lista[2]);
+
+    let mut numeros: Vec<u8> = Vec::new();
+
+    for i in 1..=5 {
+        numeros.push(i);
+    }
+
+    println!("Valores do vetor {:?} ", numeros);
+
+    for n in numeros {
+        println!("{}", n);
+    }
+
+    println!(" ");
+}
